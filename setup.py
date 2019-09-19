8369791cefa101b2356b8dab485483b8a7645915
EiffL <fr.eiffel@gmail.com>
2019-09-18 18:28:44 -0700

    Moved code around

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='flowpm',
+      version='0.1',
+      description='Particle Mesh Simulation in TensorFlow',
+      url='https://github.com/modichirag/flowpm',
+      author='Chirag Modi',
+      author_email='modichirag@berkeley.edu',
+      license='MIT',
+      packages=['flowpm'],
+      zip_safe=False)
