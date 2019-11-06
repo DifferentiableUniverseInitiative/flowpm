948d1d5f4ce502e4175367adb0ff7523d7d4a47a
EiffL <fr.eiffel@gmail.com>
2019-11-06 02:17:25 -0800

    Changes default mesh

--- scripts/cori_benchmark_job.sh
+++ scripts/cori_benchmark_job.sh
@@ -7,4 +7,4 @@
 #SBATCH --exclusive -A m1759
 module purge && module load tensorflow/gpu-1.15.0-rc1-py37 esslurm gcc/7.3.0 cuda mvapich2
 
-srun python fft_benchmark.py --cube_size=1024 --batch_size=1 > log_1024
+srun python fft_benchmark.py --cube_size=128 --batch_size=1 --mesh_shape="b1:16" --layout="nx:b1" > log_128
