4e448bf45851ccfa1b0ec85cc978a38361cefef4
EiffL <fr.eiffel@gmail.com>
2020-01-26 08:53:37 -0800

    Working baseline

--- examples/pyramid_job.sh
+++ examples/pyramid_job.sh
@@ -5,5 +5,5 @@
 #SBATCH --time=10
 #SBATCH --gres=gpu:2
 #SBATCH --exclusive -A m1759
-module purge && module load  tensorflow/gpu-2.0.0-py37 esslurm gcc/7.3.0 cuda
+module purge && module load  esslurm gcc/7.3.0 python3 cuda/10.1.243
 srun python pyramid_nbody_SLURM.py --nc=128 > log-play
