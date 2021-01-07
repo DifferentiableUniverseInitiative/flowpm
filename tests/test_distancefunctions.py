3fdc468a054d7ec5828bbda46368ef4af2546e30
EiffL <fr.eiffel@gmail.com>
2021-01-08 00:53:17 +0100

    fix mismatch between cosmologies

--- tests/test_distancefunctions.py
+++ tests/test_distancefunctions.py
@@ -14,36 +14,36 @@ from nbodykit.cosmology import Planck15 as cosmo
 cosmo1={"w0":-1.0,
         "wa":0.0,
         "H0":100,
-        "h":0.6774,
-        "Omega0_b":0.04860,
-        "Omega0_c":0.2589,
-        "Omega0_m":0.3075,
+        "h":cosmo.h,
+        "Omega0_b":cosmo.Omega0_b,
+        "Omega0_c":cosmo.Omega0_cdm,
+        "Omega0_m":cosmo.Omega0_m,
         "Omega0_k":0.0,
-        "Omega0_de":0.6925,
-        "n_s":0.9667,
-        "sigma8":0.8159}
+        "Omega0_de":cosmo.Omega0_lambda,
+        "n_s":cosmo.n_s,
+        "sigma8":cosmo.sigma8}
 
-    
-    
+
+
 def test_radial_comoving_distance():
     """ This function tests the function computing the radial comoving distance.
     """
     a = np.logspace(-2, 0.0)
-    
+
     z=1/a-1
-    
+
     radial =rad_comoving_distance(cosmo1,a)
-    
+
     radial_astr=cosmo.comoving_distance(z)
-    
+
     assert_allclose(radial,radial_astr,rtol=1e-2)
-    
-    
+
+
 def test_transverse_comoving_distance():
       """This function test the function computing the Transverse comoving distance in [Mpc/h] for a given scale factor
       """
       a = np.logspace(-2, 0.0)
-      
+
       z=1/a-1
 
       trans_tf=trans_comoving_distance(cosmo1,a)
@@ -56,20 +56,20 @@ def test_transverse_comoving_distance():
 def test_angular_diameter_distance():
     """This function test the function computing the  Angular diameter distance in [Mpc/h] for a given scale factor
       """
- 
+
     a=np.logspace(-2,0.0)
-    
+
     z=1/a-1
-    
+
     angular_diameter_distance_astr=cosmo.angular_diameter_distance(z)
-    
+
     angular_diameter_distance_tf=ang_diameter_distance(cosmo1,a)
-    
+
     assert_allclose(angular_diameter_distance_tf, angular_diameter_distance_astr,rtol=1e-2)
- 
+
 # =============================================================================
-# Here we use the nbodykit function that compute comoving_distance as a function of a/z to  
-# build a new a-of-chi function by interpolation using a scipy interpolation function. 
+# Here we use the nbodykit function that compute comoving_distance as a function of a/z to
+# build a new a-of-chi function by interpolation using a scipy interpolation function.
 # Then we compare thiss function with our a-of-chi function.
 # =============================================================================
 from scipy import interpolate
@@ -93,26 +93,26 @@ def a_of_chi(z):
     """
     a=1/(1+z)
     cache_chi=cosmo.comoving_distance(z)
-    return interpolate.interp1d(cache_chi, a)
+    return interpolate.interp1d(cache_chi, a, kind='cubic')
 
 
-def test_a_of_chi(): 
+def test_a_of_chi():
     """This function test the function computing the scale factor for corresponding (array) of radial comoving
     distance by reverse linear interpolation
-      """ 
-      
-    a = np.logspace(-2, 0.0)
-    
+      """
+
+    a = np.logspace(-2, 0.0, 512)
+
     z=1/a-1
-    
+
     chi = np.geomspace(500, 8000, 50)
-      
+
     aofchi_tf=a_of_chi_tf(cosmo1,chi)
-    
+
     f=a_of_chi(z)
-      
+
     aofchi_astr=f(chi)
-      
+
     assert_allclose(aofchi_tf,aofchi_astr,rtol=1e-2)
 
 # from  jax_cosmo.background import a_of_chi as a_of_chi_jax
@@ -120,40 +120,37 @@ def test_a_of_chi():
 # cosmo_jax= jc.Planck15()
 
 
-# def test_a_of_chi2(): 
+# def test_a_of_chi2():
 #     """This function test the function computing the scale factor for corresponding (array) of radial comoving
 #     distance by reverse linear interpolation
-#       """ 
-     
+#       """
+
 #     #Comparing a-of-chi jax with a of chi scipy
 #     a = np.logspace(-2, 0.0)
-    
+
 #     z=1/a-1
-    
+
 #     chi = np.linspace(500, 8000, 50)
-      
+
 #     aofchi_jax=a_of_chi_jax(cosmo_jax,chi)
-    
+
 #     f=a_of_chi(z)
-      
+
 #     aofchi_astr=f(chi)
-      
+
 #     assert_allclose(aofchi_jax,aofchi_astr,rtol=1e-2)
 
 
-# def test_a_of_chi3(): 
+# def test_a_of_chi3():
 #     """This function test the function computing the scale factor for corresponding (array) of radial comoving
 #     distance by reverse linear interpolation
-#       """ 
+#       """
 #       #Comparing a-of-chi jax with a of chi tensorflow
-    
+
 #     chi = np.linspace(500, 8000, 50)
-      
+
 #     aofchi_jax=a_of_chi_jax(cosmo_jax,chi)
-    
+
 #     aofchi_tf=a_of_chi_tf(cosmo1,chi)
-      
+
 #     assert_allclose(aofchi_jax,aofchi_tf,rtol=1e-2)
-
-
-
