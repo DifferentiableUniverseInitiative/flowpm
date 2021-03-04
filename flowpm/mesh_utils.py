2f7662862d5e334fa32537f919bec47a5679c9b5
EiffL <fr.eiffel@gmail.com>
2021-03-04 19:48:08 +0100

    forgot one merge

--- flowpm/mesh_utils.py
+++ flowpm/mesh_utils.py
@@ -107,11 +107,7 @@ def _cic_readout(mesh, neighboor_coords, kernel, shift, name=None):
     mesh = mesh[:, 0, 0, 0]
     shape_part = tf.shape(neighboor_coords)
 
-<<<<<<< HEAD
-    #TODO: Assert shift shape
-=======
     # TODO: Assert shift shape
->>>>>>> origin/master
     neighboor_coords = tf.reshape(neighboor_coords, (-1, 8, 4))
     neighboor_coords = neighboor_coords + tf.reshape(tf.constant(shift),
                                                      [1, 1, 4])
