ac1271ae9c4c5410f8728fdf9769595320c7d105
EiffL <fr.eiffel@gmail.com>
2021-03-08 00:44:09 +0100

    Fix yapf formatting

--- docs/conf.py
+++ docs/conf.py
@@ -14,13 +14,11 @@
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
-
 # -- Project information -----------------------------------------------------
 
 project = 'FlowPM'
 copyright = '2021, FlowPM Developers'
 author = 'FlowPM Developers'
-
 
 # -- General configuration ---------------------------------------------------
 
@@ -45,7 +43,6 @@ templates_path = ['_templates']
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
-
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
@@ -57,7 +54,6 @@ html_theme = 'sphinx_rtd_theme'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
-
 
 autosummary_generate = True
 autoclass_content = 'both'
