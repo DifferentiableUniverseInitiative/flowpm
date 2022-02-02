100c1558f0166ae3b2c7f6ff9b91c40ddf2a02f4
Denise Lanzieri <denise.lanzieri@cea.fr>
2022-02-02 11:44:06 -0800

    function for tomogr

--- flowpm/redshift.py
+++ flowpm/redshift.py
@@ -1,49 +1,113 @@
 import tensorflow as tf
-from flowpm.scipy.integrate import simps
+import flowpm.scipy.integrate as integrate
+import flowpm.scipy.interpolate as interpolate
+import flowpm
+from astropy.io import fits
+from flowpm.NLA_IA import k_IA
+import astropy.units as u
+import numpy as np
 
 
-def smail_nz(z, a, b, z0):
-  """Defines a smail distribution with these arguments
+def LSST_Y1_tomog(cosmology,
+                  lensplanes,
+                  box_size,
+                  z_source,
+                  field_npix,
+                  field_size,
+                  nbin,
+                  batch_size,
+                  use_A_ia=False,
+                  Aia=None):
+  """This function takes as input a list of lensplanes and redshift distribution and returns a stacked convergence maps for each tomographic bin) 
     
     Parameters:
     -----------
     
-    z: array_like or tf.TensorArray
-       Photometric redshift
-        
-             
-    a: float value
-        Parameter of Smail distribution
+    cosmology: `Cosmology`,
+        cosmology object.
     
-    b: float value
-        Parameter of Smail distribution
+    lensplanes: list of tuples (r, a, density_plane),
+        lens planes to use
     
-    z0: float value
-        Parameter of Smail distribution
+    boxsize: float 
+        Transverse comoving size of the simulation volume [Mpc/h]
+
+    z_source: array_like or tf.TensorArray
+       Redshift of the source plane
+    
+    field_npix: Int
+        Resolution of the final interpolated plane
+    
+    nbin: float.
+        Number of photometric bins to use.
         
-    Notes
-    -----
-
-    The expression for :math:`n(z)` is:
-
-    .. math::
-
-      \n(z)=z^{a}\exp{-(z/z_0)^b}
+    batch_size: int
+        Size of batches
+        
+    use_A_ia: Boolean
+        If true, the frunction will return the stack convergence map for the IA signal,
+        if false, the stack convergence map for the lensing signal
+    
+    Aia: Float or None (default)
+        Amplitude parameter AI, describes the  strength of the tidal coupling. 
+    
+    Returns
+    -------
+    tom_kappa: tf.TensorArray [nbins,field_npix,field_npix]
+    Stacked convergence maps for each tomographic bin
+    
+    Note:
+    -------
+    Details of the redshift distribution used can be found in this paper: https://arxiv.org/pdf/2111.04917.pdf 
     """
 
-  smail_nz = []
-  zmax = 10.0
-
-  def smail(z, a, b, z0):
-    return (z)**a * tf.math.exp(-(((z) / z0)**b))
-
-  for i in range(len(z0)):
-    norm = simps(lambda t: smail(t, a, b, z0[i]), 0.0, 10, 256)
-    norm_smail = tf.cast(
-        smail(z, a, b, z0[i]), dtype=tf.float32) / tf.cast(
-            norm, dtype=tf.float32)
-    smail_nz.append(norm_smail)
-  return tf.stack(smail_nz, axis=0)
+  xgrid, ygrid = np.meshgrid(
+      np.linspace(0, field_size, field_npix,
+                  endpoint=False),  # range of X coordinates
+      np.linspace(0, field_size, field_npix,
+                  endpoint=False))  # range of Y coordinates
+  coords = np.stack([xgrid, ygrid], axis=0) * u.deg
+  c = coords.reshape([2, -1]).T.to(u.rad)
+  hdul = fits.open('/global/homes/d/dlan/flowpm/notebooks/lsst_y1.fits')
+  data = hdul[1].data
+  norm = integrate.simps(lambda t: (t**2) * np.exp(-((t / 0.26)**0.94)),
+                         min(data['Z']), max(data['Z']))
+  tom_kappa = []
+  if use_A_ia is not False:
+    for i in range(nbin):
+      nz_source = interpolate.interp_tf(
+          z_source, data['Z'],
+          tf.cast(data["BIN_%s " % (i)] * norm, dtype=tf.float32))
+      sum_kappa = 0
+      for j in range(len(z_source)):
+        im_IA = flowpm.raytracing.interpolation(
+            lensplanes[j][-1],
+            dx=box_size / 2048,
+            r_center=lensplanes[j][0],
+            field_npix=field_npix,
+            coords=c)
+        k_ia = k_IA(cosmology, lensplanes[j][1], im_IA, Aia)
+        sum_kappa = sum_kappa + (nz_source[j] * k_ia[0])
+      tom_kappa.append(sum_kappa)
+  else:
+    for i in range(nbin):
+      nz_source = interpolate.interp_tf(
+          z_source, data['Z'],
+          tf.cast(data["BIN_%s " % (i)] * norm, dtype=tf.float32))
+      sum_kappa = 0
+      for j in range(len(z_source)):
+        m = flowpm.raytracing.convergenceBorn(
+            cosmology,
+            lensplanes,
+            dx=box_size / 2048,
+            dz=box_size,
+            coords=c,
+            z_source=z_source[j],
+            field_npix=field_npix)
+        m = tf.reshape(m, [batch_size, field_npix, field_npix])
+        sum_kappa = sum_kappa + (nz_source[j] * m[0])
+      tom_kappa.append(sum_kappa)
+  return tf.stack(tom_kappa, axis=0)
 
 
 def systematic_shift(z, bias):
